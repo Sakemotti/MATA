4eeaec40b4d7cf786373331e0c789224a470ca14
Sakemotti <aassdd123123000@gmail.com>
2026-09-01 20:35:39 +0900

    test: migrate Compose rules to v2 API

--- app/src/androidTest/java/com/mochisofts/mata/ui/calendar/CalendarHistoryRowTest.kt
+++ app/src/androidTest/java/com/mochisofts/mata/ui/calendar/CalendarHistoryRowTest.kt
@@ -3,7 +3,7 @@ package com.mochisofts.mata.ui.calendar
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.width
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.v2.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.unit.dp
 import com.mochisofts.mata.core.designsystem.MataTheme
