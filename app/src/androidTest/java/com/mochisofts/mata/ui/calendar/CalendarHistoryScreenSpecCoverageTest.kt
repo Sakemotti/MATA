79110e15945bc9fdf9642ac12184aa8de1f863c5
Sakemotti <aassdd123123000@gmail.com>
2026-09-13 18:55:15 +0900

    test: import snackbar node query

--- app/src/androidTest/java/com/mochisofts/mata/ui/calendar/CalendarHistoryScreenSpecCoverageTest.kt
+++ app/src/androidTest/java/com/mochisofts/mata/ui/calendar/CalendarHistoryScreenSpecCoverageTest.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.junit4.v2.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
